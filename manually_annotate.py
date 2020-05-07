e96f45e37410ad3392f912a37fb4cf7b4a90b84d
Shreyas <shreyas2206@gmail.com>
2020-05-07 16:53:39 +0530

    for shared annotation

--- manually_annotate.py
+++ manually_annotate.py
@@ -11,6 +11,7 @@ import sys
 import numpy as np
 import subprocess
 import pandas as pd
+from pdb import set_trace as bp
 
 # from playsound import playsound
 # """
@@ -33,13 +34,38 @@ def getUserInput(question, uttid):
                 sys.exit()
     return a, finish
 
-def saveProgress(finish, responses, annot_csvfile):
+def saveProgress(finish, responses, annot_csvfile, utt2annotator, anotator_dict, done_dict):
     if finish:
         responses = pd.DataFrame(np.asarray(responses[:-1])).to_csv(index=None,header=None)
         with open(annot_csvfile,'a+') as f:
             f.write(responses)
+        write_utt2annotator(utt2annotator, anotator_dict, done_dict) 
         sys.exit()
+        
+        
+def load_annotator_info(utt2annotator):
+    annotatorcsv = np.genfromtxt(utt2annotator,dtype='str',delimiter=',', skip_header=1)
+    anotator_dict = dict(zip(annotatorcsv[:,0],annotatorcsv[:,1]))
+    done_dict = dict(zip(annotatorcsv[:,0],annotatorcsv[:,2]))
+    return annotatorcsv, anotator_dict, done_dict
 
+def get_uttids_to_annotate(wavscp, existing_uttids_in_annot, anotator_dict):
+    n_utts = int(input("How many utterances will you annotate now? :"))
+    utts = []
+    i = 0
+    for uttid in wavscp[:,0]:
+        if uttid not in existing_uttids_in_annot:
+            if uttid not in anotator_dict.keys():
+                i+=1
+                if i>n_utts:
+                    break
+                utts.append(uttid)
+    return utts
+
+def write_utt2annotator(utt2annotator, anotator_dict, done_dict):
+    a = np.asarray([[utt, a, done_dict[utt]] for utt,a in anotator_dict.items()])
+    np.savetxt(utt2annotator,a,fmt='%s',delimiter=',',comments='',header='uttid,annotator,done')
+    
 def main():
     wavscp = np.genfromtxt('data/wav.scp', dtype='str')
     annot_csvfile = 'data/annot.csv'
@@ -50,13 +76,19 @@ def main():
         raise("Check annot csv file path")
     
     if os.path.exists(utt2annotator):
-        annotatorcsv = np.genfromtxt(utt2annotator,dtype='str',delimiter=',')
+        annotatorcsv, anotator_dict, done_dict = load_annotator_info(utt2annotator)
     else:
         raise("Check utt2annotator path!")
 
     annotator = input("Who is annotating? : ")
     remaining_utts_idx  = (annotatorcsv[:,1]==annotator)*(annotatorcsv[:,2]=='n')
-    
+    remaining_utts = annotatorcsv[:,0][remaining_utts_idx]
+    if remaining_utts.size == 0:
+        remaining_utts = get_uttids_to_annotate(wavscp, existing_uttids_in_annot, anotator_dict)
+        anotator_dict.update(dict(zip(remaining_utts,[annotator for u in remaining_utts])))
+        done_dict.update(dict(zip(remaining_utts,['n' for u in remaining_utts])))
+        write_utt2annotator(utt2annotator, anotator_dict, done_dict)
+        
     
     questions = ["Is audio present? (y/n) : ",
                  "Is the file clean? (y/n) : ",
@@ -77,6 +109,10 @@ def main():
     for uttid, wavfile in wavscp:
         if uttid in existing_uttids_in_annot:
             continue
+        if uttid not in anotator_dict.keys():
+            continue
+        if anotator_dict[uttid]!=annotator:
+            continue
         responses.append([uttid])
         print("Playing {}:".format(uttid))
         playsound(wavfile)
@@ -94,14 +130,16 @@ def main():
                     playsound(wavfile)
                 else:
                     print("Invalid Input. Try again \n")
-            saveProgress(finish, responses, annot_csvfile)
+            saveProgress(finish, responses, annot_csvfile, utt2annotator, anotator_dict, done_dict)
         a, finish = getUserInput("Any other comments?", uttid)
-        saveProgress(finish, responses, annot_csvfile)
+        done_dict[uttid]='y'
+        saveProgress(finish, responses, annot_csvfile, utt2annotator, anotator_dict, done_dict)
         responses[-1].append(a.replace(',','.'))
     
     responses = pd.DataFrame(np.asarray(responses)).to_csv(index=None,header=None)
     with open(annot_csvfile,'a+') as f:
         f.write(responses)
+    write_utt2annotator(utt2annotator, anotator_dict, done_dict)
     print("Done annotating files assigned to {}.".format(annotator))
     a = np.genfromtxt(annot_csvfile, dtype='str',delimiter=',').T[0][1:]
     b = np.genfromtxt('data/wav.scp',dtype='str')
